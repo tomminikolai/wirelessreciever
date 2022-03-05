75a490531f206911c644ed781c2a4db65231ed4c
Tommi tofthøj <ttofthoj@gmail.com>
2022-03-05 14:33:44 +0100

    add wirelessreciever.py

---
+++ wirelessreciever.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"connection from {address} has been achieved")
+    clientsocket.send(bytes("welcome", "utf-8"))
