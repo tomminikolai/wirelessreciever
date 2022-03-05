b9f98321bc116bc59f432bbc82dc2b7a2910c19c
Tommi tofthøj <ttofthoj@gmail.com>
2022-03-05 16:33:25 +0100

    update wirelessreciever.py

--- wirelessreciever.py
+++ wirelessreciever.py
@@ -1,9 +1,10 @@
 import socket
+import serial
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
-
+ser1 = serial.Serial('/dev/ttyUSB0')
 while True:
     clientsocket, address = s.accept()
     print(f"connection from {address} has been achieved")
@@ -11,3 +12,5 @@ while True:
     while True:
         msg = clientsocket.recv(1024)
         print(msg)
+        ser1.write(msg)
+
