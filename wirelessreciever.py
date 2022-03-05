62779d25cc538113513cb4596d9fdd365ce2312a
Tommi tofthøj <ttofthoj@gmail.com>
2022-03-05 15:02:36 +0100

    update wirelessreciever.py

--- wirelessreciever.py
+++ wirelessreciever.py
@@ -3,11 +3,11 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
-
+s.
 while True:
     clientsocket, address = s.accept()
     print(f"connection from {address} has been achieved")
     clientsocket.send(bytes("Welcome", "utf-8"))
     while True:
-        msg = s.recv(1024)
+        msg = clientsocket.recv(1024)
         print(msg)
